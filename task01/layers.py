32e2ad76c92f13644db928dd013a312b53e004b0
yjqiang <8900942+yjqiang@users.noreply.github.com>
2020-08-21 23:56:49 +0800

    task01交叉熵

--- task01/layers.py
+++ task01/layers.py
@@ -16,7 +16,7 @@ class LinearLayer:
     def __init__(self, input_features: int, out_features: int, weight_scale: float = 1e-3):
         self.w = np.random.randn(input_features, out_features) * weight_scale  # 随机初始化
         self.b = np.zeros(out_features)
-        self.x = None
+        self.x = None  # 用于反向传播
 
     def forward(self, x: np.ndarray) -> np.ndarray:
         """
@@ -25,7 +25,7 @@ class LinearLayer:
         :return: shape: (N, out_features)
         """
         self.x = x
-        return x @ self.w + self.b
+        return LinearLayer.for_grad_check(self.w, self.b, x)
 
     def backward(self, dout: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
         """
@@ -49,3 +49,55 @@ class LinearLayer:
         """
         return x @ w + b
 
+
+class CrossEntropyLossLayer:
+    """
+    交叉熵
+    """
+    def __init__(self):
+        self.dx = None  # 用于反向传播；最后一步了，直接就能求了
+
+    def forward(self, x: np.ndarray, y: np.ndarray) -> float:
+        """
+        前向传播
+        :param x: shape: (N, num_of_classes) x 就是 scores
+        :param y: shape: (N, )   y[i] 是 x[i] 的分类真值，且 0 <= y[i] < num_of_classes
+        :return: loss
+        """
+        N = x.shape[0]
+
+        shifted_x = x - np.max(x, axis=1, keepdims=True)
+        exp_scores = np.exp(shifted_x)
+        ps = np.sum(exp_scores, axis=1).reshape((-1, 1))
+        loss = (- np.sum(shifted_x[np.arange(N), y]) + np.sum(np.log(ps))) / N
+
+        # 顺手求 dx 因为，exp_scores 和 ps 直接用很舒服；而且最后一步了，直接就能求了
+        dx = exp_scores / ps
+        dx[np.arange(N), y] -= 1
+        dx /= N
+        self.dx = dx
+        return loss
+
+    def backward(self) -> np.ndarray:
+        """
+        反向传播
+        :return:
+        """
+        return self.dx
+
+    @staticmethod
+    def for_grad_check(x: np.ndarray, y: np.ndarray) -> float:
+        """
+        前向传播的相同表示，用于梯度检查（我这里也想复用，可 forward 中 exp_scores 和 ps 这些都得用到阿）
+        :param x: shape: (N, num_of_classes) x 就是 scores
+        :param y: shape: (N, )   y[i] 是 x[i] 的分类真值，且 0 <= y[i] < num_of_classes
+        :return: loss
+        """
+        N = x.shape[0]
+
+        shifted_x = x - np.max(x, axis=1, keepdims=True)
+        exp_scores = np.exp(shifted_x)
+        ps = np.sum(exp_scores, axis=1).reshape((-1, 1))
+        loss = (- np.sum(shifted_x[np.arange(N), y]) + np.sum(np.log(ps))) / N
+
+        return loss
--- task01/test_gradient_check.py
+++ task01/test_gradient_check.py
@@ -6,7 +6,7 @@ import gradient_check
 
 input_features = 87
 out_features = 57
-x = np.random.randn(10, input_features) * 1.5
+x = np.random.randn(10, input_features)
 layer = layers.LinearLayer(input_features, out_features)
 out = layer.forward(x)
 
@@ -16,3 +16,14 @@ dx, dw, db = layer.backward(dout)
 gradient_check.eval_numerical_gradient_array(lambda w_: layers.LinearLayer.for_grad_check(x=x, w=w_, b=layer.b), layer.w, dw, dout)
 gradient_check.eval_numerical_gradient_array(lambda x_: layers.LinearLayer.for_grad_check(x=x_, w=layer.w, b=layer.b), x, dx, dout)
 gradient_check.eval_numerical_gradient_array(lambda b_: layers.LinearLayer.for_grad_check(x=x, w=layer.w, b=b_), layer.b, db, dout)
+
+########################################################################
+
+
+layer = layers.CrossEntropyLossLayer()
+x = np.random.randn(10, 21)
+y = np.random.randint(21, size=(10, ))
+
+out = layer.forward(x, y)
+dx = layer.backward()
+gradient_check.grad_check_sparse(lambda x_: layers.CrossEntropyLossLayer.for_grad_check(x=x, y=y), x, dx)
