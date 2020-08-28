c670176ad7c44a33ab0c819aee5c815f27fd9e0a
yjqiang <8900942+yjqiang@users.noreply.github.com>
2020-08-28 16:36:50 +0800

    fix bug

--- task01/tmp.py
+++ task01/tmp.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import sys
 
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 import data_handle
 import features
@@ -16,13 +17,13 @@ LEARNING_RATE = 1e-3
 # 如果没有分割好的数据，那么就进行分割（否则每次运行，x_train 数据不一样，词袋模型的生成词典大小也不一样，那些全连接层参数大小不固定）
 if not (Path("data/train_split.tsv").is_file() and Path("data/val_split.tsv").is_file()):
     sentences, labels = data_handle.read_tsv_data0('data/train.tsv')  # 读取数据
-    x_train_orig, x_val_orig, y_train, y_val = train_test_split(sentences, labels, test_size=0.25)  # 按照 75: 25 比例划分 训练集和验证集；结果的 type 不变，仍为 pd.Series
-    data_handle.save_tsv_data([x_train_orig, y_train], 'data/train_split.tsv')
-    data_handle.save_tsv_data([x_val_orig, y_val], 'data/val_split.tsv')
+    x_train_orig, x_val_orig, y_train_orig, y_val_orig = train_test_split(sentences, labels, test_size=0.25)  # 按照 75: 25 比例划分 训练集和验证集；结果的 type 不变，仍为 pd.Series
+    data_handle.save_tsv_data([x_train_orig, y_train_orig], 'data/train_split.tsv')
+    data_handle.save_tsv_data([x_val_orig, y_val_orig], 'data/val_split.tsv')
 else:
-    x_train_orig, y_train = data_handle.read_tsv_data0('data/train_split.tsv')
-    x_val_orig, y_val = data_handle.read_tsv_data0('data/val_split.tsv')
-    x_val_orig, y_val = x_val_orig[:3000], y_val[:3000]
+    x_train_orig, y_train_orig = data_handle.read_tsv_data0('data/train_split.tsv')
+    x_val_orig, y_val_orig = data_handle.read_tsv_data0('data/val_split.tsv')
+    x_val_orig, y_val_orig = x_val_orig[:3000], y_val_orig[:3000]
 
 n_gram = 1
 
@@ -31,6 +32,9 @@ n_gram = 1
 bog = features.BagOfWordFeature(n_gram=n_gram)
 x_train = bog.fit(x_train_orig)
 x_val = bog.transform(x_val_orig)
+
+y_train = np.asarray(y_train_orig)
+y_val = np.asarray(y_val_orig)
 print(f'训练集：{x_train.shape=}, {y_train.shape=}')
 print(f'验证集：{x_val.shape=}, {y_val.shape=}')
 
