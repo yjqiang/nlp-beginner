0d19511f81a720ca3490c146a35fc8ff3322cafd
yjqiang <8900942+yjqiang@users.noreply.github.com>
2020-09-04 01:10:12 +0800

    task02 预处理数据
    否则文件处理花费大量时间，而且还占用大量内存

--- task03/data_handle.py
+++ task03/data_handle.py
@@ -1,5 +1,6 @@
 import json
-from typing import List, Tuple
+from typing import List, Tuple, Dict, Any
+import pickle
 
 
 def read_data(path: str) -> Tuple[List[str], List[str], List[int]]:
@@ -23,3 +24,27 @@ def read_data(path: str) -> Tuple[List[str], List[str], List[int]]:
             x2.append(line['sentence2'])
             labels.append(label2index[line['gold_label']])
     return x1, x2, labels
+
+
+# 读取 glove 文件
+def read_glove(path: str) -> Tuple[List[List[float]], Dict[str, int]]:
+    embedding = []
+    word2id = {}
+    with open(path, 'r', encoding='UTF-8') as f:
+        for index, line in enumerate(f):
+            line = line.rstrip('\n')  # remove the newline character
+            if line:  # 移除空白行
+                list_line = line.split()
+                embedding.append([float(value) for value in list_line[1:]])
+                word2id[list_line[0]] = index  # word = list_line[0]
+    return embedding, word2id
+
+
+def save_pickle(path: str, data: Any) -> None:
+    with open(path, 'wb') as f:
+        pickle.dump(data, f)
+
+
+def load_pickle(path: str) -> Any:
+    with open(path, 'rb') as f:
+        return pickle.load(f)
---
+++ task03/pre_handle_data.py
@@ -0,0 +1,35 @@
+"""
+把数据进行提取转化，否则炸。。。
+"""
+
+from typing import List, Tuple
+
+import main
+import data_handle
+from utils import utils
+
+
+def read_and_transform_data(path: str) -> Tuple[List[List[int]], List[List[int]], List[int]]:
+    """
+    读取 snli 文件并且转化文件
+    :param path:
+    :return:
+    """
+    x1_orig, x2_orig, y_orig = data_handle.read_data(path)
+
+    # 处理数据
+    x1_sentences_str = utils.split_sentences(x1_orig)
+    x1_sentences_int = main.VOCAB.convert2indices(x1_sentences_str)
+    x2_sentences_str = utils.split_sentences(x2_orig)
+    x2_sentences_int = main.VOCAB.convert2indices(x2_sentences_str)
+    return x1_sentences_int, x2_sentences_int, y_orig
+
+
+if __name__ == '__main__':
+    snli_path = 'data/snli_1.0'
+    result = read_and_transform_data(f'{snli_path}/snli_1.0_dev.jsonl')
+    data_handle.save_pickle(f'{snli_path}/snli_1.0_dev.pkl', result)
+    result = read_and_transform_data(f'{snli_path}/snli_1.0_test.jsonl')
+    data_handle.save_pickle(f'{snli_path}/snli_1.0_test.pkl', result)
+    result = read_and_transform_data(f'{snli_path}/snli_1.0_train.jsonl')
+    data_handle.save_pickle(f'{snli_path}/snli_1.0_train.pkl', result)
